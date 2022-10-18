22f541f7ebc31d38616cf140877e4ca2bc2dc473
Benjamin Morrison <bencmorrison@pm.me>
2022-10-18 22:11:35 +1100

    Initial Commit

---
+++ Sources/Support/Support.swift
@@ -0,0 +1,6 @@
+public struct Support {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SupportTests/SupportTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Support
+
+final class SupportTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Support().text, "Hello, World!")
+    }
+}
